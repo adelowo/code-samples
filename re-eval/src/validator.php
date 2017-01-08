b625c5c3606de6272cc99574d1e834d62be3f98d
Lanre Adelowo <me@lanreadelowo.com>
2017-01-08 05:09:09 +0100

    Validator plus tests

---
+++ re-eval/src/validator.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Adelowo\Reeval;
+
+use Countable;
+use Exception;
+use InvalidArgumentException;
+
+function validator()
+{
+
+    return new class
+    {
+
+        public function __construct()
+        {
+            $this->errors = errorBag();
+        }
+
+        public function passes()
+        {
+            return !$this->fails();
+        }
+
+        public function fails()
+        {
+            return $this->errors->count() > 0;
+        }
+
+        public function getErrors()
+        {
+            return $this->errors;
+        }
+    };
+}
+
+function errorBag(array $defaultErrors = [])
+{
+
+    return new class implements Countable
+    {
+
+        protected $values;
+
+        public function __construct(array $defaultErrors = [])
+        {
+            $this->values = $defaultErrors;
+        }
+
+        public function add(string $index, string $message)
+        {
+            $this->values[$index] = $message;
+        }
+
+        public function get(string $index)
+        {
+
+            if ($this->has($index)) {
+                return $this->values[$index];
+            }
+
+            throw new InvalidArgumentException(
+                "{$index} does not exst in this bag"
+            );
+        }
+
+        public function has(string $index)
+        {
+            return array_key_exists($index, $this->all());
+        }
+
+        public function count()
+        {
+            return count($this->values);
+        }
+
+        public function all()
+        {
+            return $this->values;
+        }
+    };
+}
+
+function validate(array $rules)
+{
+    $validator = validator();
+    $errorBag = $validator->getErrors();
+
+    foreach ($rules as $rule) {
+
+        $parsedRules = parse_validator_rules($rule);
+
+        foreach ($parsedRules['rules'] as $parsedRule) {
+
+            switch ($parsedRule[0]) {
+
+                case "length":
+                    validateLengthRule($parsedRules, $errorBag);
+                    continue;
+
+                case "email" :
+                    validateEmailRule($parsedRules, $errorBag);
+                    continue;
+
+                default :
+                    throw_unknown_rule_exception($parsedRule[0]);
+            }
+        }
+    }
+
+    return $validator;
+}
+
+function parse_validator_rules(string $index)
+{
+
+    $explodedRule = explode(":", $index);
+
+    $index = $explodedRule[0];
+
+    foreach (explode(",", $explodedRule[1]) as $rule) {
+        $rules[] = explode("=>", $rule);
+    }
+
+    return [
+        "index" => $index,
+        "rules" => $rules
+    ];
+}
+
+
+function validateLengthRule(array $ruleData, &$errorBag)
+{
+    $index = $ruleData['index'];
+
+    //could have resorted to `list()` here but not all length rules would specify a max value.
+    //This is to prevent an index error.
+    //index 0 would hold the minimum while index 1 would hold the max value - if any was given
+    $minAndMax = explode("|", $ruleData['rules'][0][1]);
+
+    if (mb_strlen($_POST[$index]) < $minAndMax[0]) {
+        $errorBag->add(
+            $index,
+            "The field, {$index} should not contain a value lower than {$minAndMax[0]} in length"
+        );
+    }
+
+    if (isset($minAndMax[1])) {
+        if (mb_strlen($_POST[$index]) > $minAndMax[1]) {
+            $errorBag->add(
+                $index,
+                "The field, {$index} should not contain a value greater than {$minAndMax[1]} in length"
+            );
+        }
+    }
+}
+
+function validateEmailRule(array $ruleData, &$errorBag)
+{
+    $index = $ruleData['index'];
+
+    if (!filter_var($_POST[$index], FILTER_VALIDATE_EMAIL)) {
+        $errorBag->add($index, "Please provide a valid email address");
+    }
+}
+
+function throw_unknown_rule_exception(string $ruleName)
+{
+    throw new Exception(
+        "The rule {$ruleName} doesn't exist on this validator. 
+        Go hunt down a library on packagist."
+    );
+}
---
+++ re-eval/tests/ReevalValidatorTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Adelowo\Reeval\Tests;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+use function Adelowo\Reeval\validate;
+use function Adelowo\Reeval\parse_validator_rules;
+
+class ReevalValidatorTest extends TestCase
+{
+
+    public function testItParsesMultipleRules()
+    {
+
+        $rule = "fullname:length=>3|50,email";
+
+        $expectedValues = [
+            "index" => "fullname",
+            "rules" => [
+                [
+                    0 => "length",
+                    1 => '3|50'
+                ],
+                [
+                    0 => "email"
+                ]
+            ]
+        ];
+
+        $this->assertSame($expectedValues, parse_validator_rules($rule));
+    }
+
+    public function testItParsesASingleRule()
+    {
+        $rule = "fullname:length=>3|50";
+
+        $expected = [
+            "index" => "fullname",
+            "rules" => [
+                [
+                    0 => "length",
+                    1 => "3|50"
+                ]
+            ]
+        ];
+
+        $this->assertSame($expected, parse_validator_rules($rule));
+    }
+
+    public function testEmailRuleWorksCorrectly()
+    {
+        $_POST['mail'] = "me@lanreadelowo.com";
+
+        $validator = validate(["mail:email"]);
+
+        $this->assertTrue($validator->passes());
+    }
+
+    public function testEmailRuleFailsBecauseOfInvalidData()
+    {
+        $_POST['mail'] = "some.ss";
+
+        $validator = validate(["mail:email"]);
+
+        $this->assertTrue($validator->fails());
+
+        $this->assertSame(1, $validator->getErrors()->count());
+    }
+
+    public function testLengthRuleWorksCorrectly()
+    {
+
+        $_POST['username'] = "therealclown";
+        $_POST['fullname'] = "Lanre Adelowo";
+        $_POST['hobby'] = "Trolling";
+
+        $validator = validate([
+            "fullname:length=>5|50",
+            "username:length=>3|20",
+            "hobby:length=>4" //rule without a max length
+        ]);
+
+        $this->assertTrue($validator->passes());
+    }
+
+    public function testLengthRuleFailsBecauseOfInvalidData()
+    {
+        $_POST['username'] = "OX";
+        $_POST['fullname'] = "Lanre";
+        $_POST['hobby'] = "naff";
+
+        $validator = validate([
+            "fullname:length=>10|50",
+            "username:length=>3|20",
+            "hobby:length=>5" //rule without a max length
+        ]);
+
+        $this->assertFalse($validator->passes());
+
+        $errors = $validator->getErrors();
+
+        $this->assertSame(3, $errors->count());
+    }
+
+    public function testMultipleRules()
+    {
+        $_POST['mail'] = "me@lanreadelowo.com";
+        $_POST['secondary_mail'] = "adelowomailbox@gmail.com";
+
+        $rules = [
+            "mail:length=>3|20,email",
+            "secondary_mail:length=>10,email"
+        ];
+
+        $validator = validate($rules);
+
+        $this->assertTrue($validator->passes());
+    }
+
+    public function testAnUnexpectedRuleIsEncountered()
+    {
+        $_POST['name'] = "Lanre Adelowo";
+
+        $rules = ["name:length=>3|60,non-existent-rule"];
+
+        $this->expectException(Exception::class);
+
+        validate($rules);
+    }
+}
